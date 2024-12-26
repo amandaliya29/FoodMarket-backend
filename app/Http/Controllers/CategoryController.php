4634c0e950e280c9e42506696b166c0c91ff2b02
Mandaliya Urvish <mandaliyaurvishj7@gmail.com>
2024-12-26 21:35:40 +0530

    adde categories api

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class CategoryController extends BaseController
+{
+    public function list()
+    {
+        try {
+            $category = Category::all();
+            return $this->sendSuccess($category, "Categories get successfully.");
+        } catch (\Throwable $th) {
+            return $this->sendError("Server Error", 500);
+        }
+    }
+
+    public function get($id)
+    {
+        try {
+            $category = Category::find($id);
+
+            if (!$category) {
+                return $this->sendError("Category not found", 404);
+            }
+
+            return $this->sendSuccess($category, "Category get successfully.");
+        } catch (\Throwable $th) {
+            return $this->sendError("Server Error", 500);
+        }
+    }
+
+    public function save(Request $request)
+    {
+        try {
+            // validation
+            $validation = Validator::make($request->all(), [
+                'name' => 'required',
+                'image' => 'required|string',
+            ]);
+
+            // validation error
+            if ($validation->fails()) {
+                return $this->sendError("Validation Error", 403);
+            }
+
+            if ($request->id) {
+                $category = Category::find($request->id);
+            } else {
+                $category = new Category();
+            }
+
+            $category->name = $request->name;
+            $category->description = $request->description;
+            $category->image = $request->image;
+
+            $category->save();
+
+            return $this->sendSuccess([], "Category details saved successfully.");
+        } catch (\Throwable $th) {
+            return $this->sendError($th->getMessage(), 500);
+        }
+    }
+
+    public function media(Request $request)
+    {
+        try {
+            $validation = Validator::make($request->all(), [
+                'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:5096',
+            ]);
+
+            // validation error
+            if ($validation->fails()) {
+                return $this->sendError("Validation Error", 403);
+            }
+
+            $url = $this->upload('categories', 'image');
+            return $this->sendSuccess(['url' => $url], "Media uploaded successfully.");
+
+        } catch (\Throwable $th) {
+            return $this->sendError("Server Error", 500);
+        }
+    }
+
+    public function delete($id)
+    {
+        try {
+            $category = Category::find($id);
+
+            if (!$category) {
+                return $this->sendError("Category not found", 404);
+            }
+
+            $category->delete();
+            return $this->sendSuccess([], "Category removed successfully.");
+
+        } catch (\Throwable $th) {
+            return $this->sendError("Server Error", 500);
+        }
+    }
+}
