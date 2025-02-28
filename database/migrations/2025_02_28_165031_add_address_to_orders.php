33498403cfc05ab270090ad8dafbf337df89c6f4
urvish mandaliya <115796297+aUrvish@users.noreply.github.com>
2025-02-28 22:51:59 +0530

    Added profile update api (#8)

---
+++ database/migrations/2025_02_28_165031_add_address_to_orders.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->string('house_no')->after('payment_type')->nullable();
+            $table->string('address')->after('house_no')->nullable();
+            $table->string('city')->after('address')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('orders', function (Blueprint $table) {
+            $table->dropColumn(['house_no', 'address', 'city']);
+        });
+    }
+};
